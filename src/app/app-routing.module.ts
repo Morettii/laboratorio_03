b30e8b5c86e921e951633089b7458c1e19ba4bc4
Morettii <paolo.moretti.jimenez@cftmail.cl>
2022-12-07 07:28:47 -0800

    control final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EgresosComponent } from './egresos/egresos.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { VerEgresosComponent } from './ver-egresos/ver-egresos.component';
+import { VerUsuariosComponent } from './ver-usuarios/ver-usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'usuarios',
+    component: UsuariosComponent
+  },
+  {
+    path: 'egresos',
+    component: EgresosComponent
+  },
+  {
+    path: 'ver_usuarios',
+    component: VerUsuariosComponent
+  },
+  {
+    path: 'ver_egresos',
+    component: VerEgresosComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
